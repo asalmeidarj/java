ce14c997c49ec1e7ce37e331659a2f7a7dfdd826
asalmeidarj <asalmeidarj@gmail.com>
2022-01-07 03:10:49 -0300

    Inserção de caixa de diálogo para nome da conta

--- ch03/Account/AccountTest2.java
+++ ch03/Account/AccountTest2.java
@@ -9,18 +9,23 @@ public class AccountTest2 {
     public static void main(String[] args) {
         
         String msg; // variável utilizada para mensagens na caixa de diálogo
+        String name; // variável utilizada para armazenar nome da conta
         double balance; // variável utilizada para armazenar saldo da conta
 
         // Criar uma instância de Account.
         Account account = new Account();
 
-        account.setName("Joaquim");
-
+        // lê nome e saldo da conta inserido pelo usuário
+        name = Dialog1.input("Insira o nome da conta: ");
         balance = Dialog1.inputNumber("Insira o saldo da conta: ");
+
+        // Settar instância de Account
+        account.setName(name);
+        account.setBalance(balance);
 
         // Variável recebe mensagem a ser exibida
         msg = "Nome da conta: " + account.getName() + "\n";
-        msg += "O saldo na conta é: " + balance;
+        msg += "O saldo na conta é: " + account.getBalance();
 
 
         Dialog1.show(msg); // exibir mensagem.
