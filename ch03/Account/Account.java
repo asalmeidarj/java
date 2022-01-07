4186271b5e73f06801b762caba303807243e8379
asalmeidarj <asalmeidarj@gmail.com>
2022-01-07 02:50:58 -0300

    Inserção de mais uma variável de instância - balance

--- ch03/Account/Account.java
+++ ch03/Account/Account.java
@@ -6,16 +6,27 @@ package ch03.Account;
 
 public class Account {
 
-    private String name; //variável de instância
+    private String name; // variável de instância (nome)
+    private double balance; // variável de instância (saldo)
 
-    //método para definir o nome objeto.
+    // método para definir o nome do objeto.
     public void setName(String name){
         this.name = name;
     }
 
-    //método para recuperar o nome do objeto.
+    // método para recuperar o nome do objeto.
     public String getName(){
         return this.name;
+    }
+
+    // método para definir saldo do objeto
+    public void setBalance(int balance){
+        this.balance = balance;
+    }
+
+    // método para recuperar o saldo do objeto
+    public double getBalance() {
+        return this.balance;
     }
 
 }
